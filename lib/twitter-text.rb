34ab633a47027a396fcb1a3100e05bbdd8e09d41
Glenn Gillen <glenn.gillen@gmail.com>
2010-03-07 22:14:54 +0000

    Adding Ruby 1.9 support.

--- lib/twitter-text.rb
+++ lib/twitter-text.rb
@@ -1,6 +1,6 @@
-raise("twitter-text requires the $KCODE variable be set to 'UTF8' or 'u'") unless ['u','UTF8'].include?($KCODE)
-
-require 'rubygems'
+if ("".respond_to?(:encoding) && "".encoding.name == "UTF-*") || (!"".respond_to?(:encoding) && !['u','UTF8'].include?($KCODE))
+  raise("twitter-text requires the $KCODE variable be set to 'UTF8' or 'u'")
+end
 
 # Needed for auto-linking
 require 'action_view'
